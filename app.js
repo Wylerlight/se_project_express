4426e993d3c5edf20a1f8f12f3ce7ba8e3971a99
Tyler Tellez <softwaretjt@gmail.com>
2023-08-15 16:37:19 -0700

    Adding mongoose, express, eslint

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const { PORT = 3001 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("app listening");
+});
